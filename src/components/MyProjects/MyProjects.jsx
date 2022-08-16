7434aab36d7afe29d20129a9384e49eca32c867e
marco94gug <marco.guglielmino94@gmail.com>
2022-08-16 20:16:34 +0200

    chore: restore project

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,20 +1,6 @@
 import './index.css'
 import ImageGallery from 'react-image-gallery';
-
-const images = [
-    {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-  ];
+import {images} from '../../constants'
 
 const MyProjects = ({myRef}) => {
 
